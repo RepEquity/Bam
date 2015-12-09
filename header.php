f063392bfaea3c4e6b3b56968e1c252c404568d8
Rene Escobar <rene@unisonagency.com>
2015-12-09 17:19:39 -0500

    add viewport meta

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 	<title>BAM</title>
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
 	<?php wp_head(); ?>
