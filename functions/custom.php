ffa88c8b96f50185f51602f82cef77b76962745e
Rene Escobar <rene@unisonagency.com>
2016-06-09 12:39:53 -0500

    cleanup old code

--- functions/custom.php
+++ functions/custom.php
@@ -3,8 +3,8 @@
 	// Changing excerpt more
 
 	function new_excerpt_more($more) {
-	global $post;
-	return '… <a href="'. get_permalink($post->ID) . '">' . '[Read More]' . '</a>';
+		global $post;
+		return '… <a href="'. get_permalink($post->ID) . '">' . '[Read More]' . '</a>';
 	}
 
 	// add_filter('excerpt_more', 'new_excerpt_more');
@@ -12,16 +12,6 @@
 	function image_alt($attachment_id){
 		$alt = trim(strip_tags( get_post_meta($attachment_id, '_wp_attachment_image_alt', true) ));
 		return $alt;
-	}
-	// Loads The Mission Statement
-	function load_mission() {
-		echo "<h2>Our Mission </h2>";
-		the_field('mission_statement', 'option');
-	}
-
-	function call_to_action($cta, $link) {
-		echo "<a class='btn btn-slide' href='". $link . "'>";
-		echo  $cta . "</a>";
 	}
 
 	function get_img_src($location) {
